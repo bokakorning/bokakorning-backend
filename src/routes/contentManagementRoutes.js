9c1e59fe86e9183c455851c981e5a186f88ae227
ranjan-2digit <ranjan@2digitinnovations.com>
2026-01-29 16:09:46 +0530

    push

--- src/routes/contentManagementRoutes.js
+++ src/routes/contentManagementRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post('/createContent',authMiddleware(['user', 'admin', 'instructer']),content.createContent);
 router.get('/getContent',content.getContent);
 router.get('/getContentForUser',content.getContentForUser);
+
 router.post('/updateContent',authMiddleware(['user', 'admin', 'instructer']),content.updateContent);
 
 module.exports = router;
